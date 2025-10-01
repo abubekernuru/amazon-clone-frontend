1fd20ece8770397e39d6b3bcecb83a3af690b2b6
abubekernuru <abubekernuru27@gmail.com>
2025-10-01 11:58:43 +0300

    cart page local storage

--- src/components/DataProvider/DataProvider.jsx
+++ src/components/DataProvider/DataProvider.jsx
@@ -1,15 +1,23 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from 'react';
 import { initialState, reducer } from "../../Utility/reducer";
 
 export const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    return (
-        <DataContext.Provider value={[state, dispatch]}>
-            {children}
-        </DataContext.Provider>
-    );
+  const [state, dispatch] = useReducer(reducer, initialState, init => {
+    try {
+      const raw = localStorage.getItem('basket');
+      return raw ? { ...init, basket: JSON.parse(raw) } : init;
+    } catch {
+      return init;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('basket', JSON.stringify(state.basket));
+  }, [state.basket]);
+
+  return <DataContext.Provider value={[state, dispatch]}>{children}</DataContext.Provider>;
 };
 
 export default DataProvider;
