7a57dc06d250e2ea00b0998f5a9de32a36589fdf
abubekernuru <abubekernuru27@gmail.com>
2025-10-07 22:26:49 +0300

    axios

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", 
+  // baseURL: "http://localhost:5000", 
+  baseURL: "https://amazon-api-deploy-2-63ta.onrender.com"
 });
 
